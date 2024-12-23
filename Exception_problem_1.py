b326e06eff538da39049480286894ceaacbb99bf
Aditya <adityakanchana@gmail.com>
2024-12-23 20:45:33 +0530

    Exception_problem_1.py

---
+++ Exception_problem_1.py
@@ -0,0 +1,14 @@
+# Handle the exception thrown by the code below by using try and except blocks.
+
+
+try:
+    for i in ['a','b','c']:
+        print(i**2)
+except:
+    print('Error Occurred')
+
+'''
+OUTPUT
+
+Error Occurred
+'''
