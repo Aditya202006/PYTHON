b669e302f2fc36fbec06f6c2c05f31b6afb47d56
Aditya <adityakanchana@gmail.com>
2024-12-23 20:46:56 +0530

    Exception_problem_2.py

---
+++ Exception_problem_2.py
@@ -0,0 +1,17 @@
+#  Handle the exception thrown by the code below by using try and except blocks. Then use a finally block to print 'All Done.'
+
+x = 5
+y = 0
+try:
+    z = x/y
+except:
+    print('Error Occurred')
+finally:
+    print('All Done')
+
+'''
+OUTPUT
+
+Error Occurred
+All Done
+'''
